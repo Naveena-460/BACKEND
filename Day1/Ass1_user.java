f24a7b487c7e7d73d5499af39cebcd0cced69a5c
Naveena-460 <naveenak460@gmail.com>
2026-01-01 21:58:34 +0530

    Update Ass1_user.java

--- Day1/Ass1_user.java
+++ Day1/Ass1_user.java
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-package com.index;
 
-import java.util.Scanner;
-
-public class ass1_user {
-
-    public static void main(String[] args) {
-
-        Scanner obj = new Scanner(System.in); 
-
-        System.out.println("Enter username:");
-        String username = obj.nextLine();
-
-        System.out.println("Enter password:");
-        String password = obj.nextLine();
-
-        if (username.equals("navee") && password.equals("1234")) {
-            System.out.println("Login Successfully");
-        } else {
-            System.out.println("Invalid Credentials");
-        }
-
-        
-    }
-}
-=======
 package com.index;
 
 import java.util.Scanner;
@@ -51,4 +25,4 @@ public class Ass1_user {
     }
 }
 
->>>>>>> 4e2be59ddc9ddbcc1590be240181a41b3a00f57a
+
