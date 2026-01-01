9f375ed541a687744f362630c001667d7ea86eff
Naveena-460 <naveenak460@gmail.com>
2026-01-01 21:57:15 +0530

    Fix merge conflict in Ass1_odd.java
    Removed merge conflict markers and cleaned up formatting.

--- Day1/Ass1_odd.java
+++ Day1/Ass1_odd.java
@@ -1,28 +1,4 @@
-<<<<<<< HEAD
-package com.index;
 
-import java.util.Scanner;
-
-public class ass1_odd {
-
-	public static void main(String[] args) {
-		System.out.println("Enter the number:");
-		Scanner obj =new Scanner(System.in);
-		int number=obj.nextInt();
-		if( number % 2 == 0 )
-		{
-		System.out.println("Even");
-		}
-		
-		else
-		{
-			System.out.println("Odd");	
-		}
-
-	}
-
-}
-=======
 package com.index;
 
 import java.util.Scanner;
@@ -47,4 +23,4 @@ public class Ass1_odd {
 
 }
 
->>>>>>> 4e2be59ddc9ddbcc1590be240181a41b3a00f57a
+
