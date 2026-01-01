4a17890781e197b07f1569451ef50b5e83a9d785
Naveena-460 <naveenak460@gmail.com>
2026-01-01 21:58:02 +0530

    Refactor Ass1_pass class and password checking logic
    Refactor password strength checker to use proper class naming conventions and improve readability.

--- Day1/Ass1_pass.java
+++ Day1/Ass1_pass.java
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-package com.index;
-import java.util.Scanner;
-public class ass1_pass {
-
-	public static void main(String[] args) {
-		
-
-        System.out.println("Enter password:");
-        Scanner sc = new Scanner(System.in);
-        String password = sc.nextLine();
-
-        if (password.length() < 8) {
-            System.out.println("Weak password");
-        } else {
-            System.out.println("Strong password");
-        }
-	}
-
-}
-=======
 package com.index;
 import java.util.Scanner;
 public class Ass1_pass {
@@ -39,4 +18,4 @@ public class Ass1_pass {
 
 }
 
->>>>>>> 4e2be59ddc9ddbcc1590be240181a41b3a00f57a
+
